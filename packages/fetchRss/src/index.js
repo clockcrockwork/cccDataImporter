5a0657354fc62c0c188339568ec30b9960e88e0e
clockcrockwork <86189309+clockcrockwork@users.noreply.github.com>
2024-06-24 10:09:57 +0900

    Update index.js remove comment

--- packages/fetchRss/src/index.js
+++ packages/fetchRss/src/index.js
@@ -55,14 +55,12 @@ function createErrorArray() {
 }
 
 async function checkForNewArticles(feedUrl, lastRetrieved) {
-    console.log('Checking for new articles:', feedUrl);
     const feed = await parser.parseURL(feedUrl);
     const newArticles = feed.items.filter(item => parseDate(item.pubDate) > parseDate(lastRetrieved));
     return newArticles;
 }
 
 async function notifyDiscord(webhookUrl, articles, webhookType, feedType) {
-    console.log('Notifying Discord:', webhookUrl);
     const payloads = articles.map(article => {
         if (feedType === SUPABASE_FEED_TYPE_X) {
             return {
@@ -96,7 +94,6 @@ async function notifyDiscord(webhookUrl, articles, webhookType, feedType) {
   
 async function processImage(imageUrl, imageName) {
     const decodedUrl = decode(imageUrl);
-    console.log('Decoded URL:', decodedUrl);
 
     try {
         const response = await fetch(decodedUrl);
@@ -129,7 +126,6 @@ async function processImage(imageUrl, imageName) {
 }
 
 async function processFeed(feed, errors) {
-    console.log('Processing feed:', feed.url);
     try {
         const lastRetrieved = feed.last_retrieved ? tzDate(feed.last_retrieved, timezone) : null;
 
@@ -226,16 +222,12 @@ async function main() {
 
     try {
         const accessToken = await authenticateUser();
-        console.log(`step: authenticateUser, accessToken: ${accessToken}`);
         const feeds = await getRssFeeds();
-        console.log(`step: getRssFeeds, feeds: ${feeds.length}`);
         const results = await Promise.allSettled(feeds.map(feed => processFeed(feed, errors)));
-        console.log(`step: processFeed, results: ${results.length}`);
         const successfulResults = results.filter(result => result.status === 'fulfilled').map(result => result.value);
 
         const updates = successfulResults.flatMap(result => result.updates);
         const notifications = successfulResults.flatMap(result => result.notifications);
-        console.log(`step: successfulResults, updates: ${updates.length}, notifications: ${notifications.length}`);
         // 現在の日時で更新
         if (updates.length > 0) {
             const currentDateTimeStr = format({
@@ -252,7 +244,6 @@ async function main() {
                 }
                 return acc;
             }, []);
-            console.log(selectMap);
 
             // updateのあるfeedをfeedMapから取得し、last_retrievedを更新
             const latestUpdates = selectMap.map(update => {
@@ -278,11 +269,8 @@ async function main() {
                 acc[webhookUrl].push({ article, webhookType, feedType });
                 return acc;
             }, {});
-            console.log(`step: groupedNotifications, groupedNotifications: ${Object.keys(groupedNotifications).length}`);
             const notificationResults = await Promise.allSettled(Object.entries(groupedNotifications).map(([webhookUrl, articles]) => notifyDiscord(webhookUrl, articles.map(({ article }) => article), articles[0].webhookType, articles[0].feedType)));
-            console.log(`step: notifyDiscord, notificationResults: ${notificationResults.length}`);
             const failedNotifications = notificationResults.filter(result => result.status === 'rejected').map(result => result.status === 'rejected' ? result.reason : null);
-            console.log(`step: failedNotifications, failedNotifications: ${failedNotifications.length}`);
             if (failedNotifications.length > 0) {
                 throw new Error(`Failed notifications: ${failedNotifications.map(err => err.message).join('\n')}`);
             }
